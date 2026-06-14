41ab74000de8de64f706f47b494a11a5456d026b
Zeteox <zeteox01@gmail.com>
2026-06-14 22:45:29 +0200

    fix: fixed imports and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import type { AgencyResponse } from "./types/api";
 import { fetchAgencies } from "./services/api";
 import Navbar from "./components/Navbar";
